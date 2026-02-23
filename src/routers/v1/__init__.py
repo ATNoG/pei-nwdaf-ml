dca8c75cae0dfff457aa5f9877a1c06d1aa752ea
Alexandre-A <aandrade2005@live.com.pt>
2026-02-23 12:19:22 +0000

    First version of the performance monitor, still somewhat manual; Fixes to model fetching when requesting inferences

--- src/routers/v1/__init__.py
+++ src/routers/v1/__init__.py
@@ -2,6 +2,7 @@ from .model_router import router as model_router
 from .fields_router import router as fields_router
 from .training_router import router as training_router
 from .inference_router import router as inference_router
+from .performance_router import router as performance_router
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -9,3 +10,4 @@ router.include_router(model_router, prefix="/models")
 router.include_router(fields_router, prefix="/fields")
 router.include_router(training_router, prefix="/training")
 router.include_router(inference_router, prefix="/inference", tags=["Inference"])
+router.include_router(performance_router, prefix="/performance", tags=["Performance"])
